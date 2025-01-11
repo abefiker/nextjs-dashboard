58d13927f6d2efcdd907912db8548e0cdf7a4bfe
abfiker <new94abemelek@gmail.com>
2025-01-11 11:10:22 +0300

    chapter-4-creating layout and pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Add the desired weights for Lusitana
+});
+
+export const inter = Inter({ 
+  subsets: ['latin'], 
+  weight: ['400', '700'] // Add the desired weights for Inter
+});
